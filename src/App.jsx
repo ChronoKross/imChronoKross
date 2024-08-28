668f2f3d511d72c1d9f799d2bbb17c05b87c2c0b
ChronoKross <nathanblainegwyn@gmail.com>
2024-08-28 00:27:02 -0400

    VORTEX BG
    added Vortex BG via https://ui.aceternity.com/components/vortex

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import "./App.css";
-import Hero from "./components/HeroCard";
-// import Footer from "./components/Footer";
-// import Navbar from "./components/Navbar";
-import TwitchCTA from "./components/TwitchCTA";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen w-full">
-      {/* <Navbar /> */}
-
-      {/* <Hero
-        title="ImChronoKross"
-        description="Still have a TON of work to do on the site. Until then slide by my Twitch and see if I'm live!"
-      /> */}
-
-      <div className="flex flex-1 justify-center items-center">
-        <TwitchCTA />
-      </div>
-
-      {/* <Footer /> */}
-    </div>
+    <div className="flex flex-col min-h-screen w-full">jzhsdfjhzdfjhujh</div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { VortexBG } from "./components/vortexBG.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <div className="text-white min-h-screen bg-black">
+  // <StrictMode>
+  <div className="text-white min-h-screen bg-black">
+    <VortexBG
+      backgroundColor="black"
+      rangeY={800}
+      particleCount={500}
+      baseHue={120}
+      className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+    >
       <App />
-    </div>
-  </StrictMode>
+    </VortexBG>
+  </div>
+  // </StrictMode>
 );
