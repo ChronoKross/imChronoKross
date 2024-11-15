50f15a508ff5068304819ab6c0881b2f8e50fdf4
ChronoKross <nathanblainegwyn@gmail.com>
2024-11-15 02:51:30 -0500

    testing
    removed posts/post components to e2e test write component

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -14,9 +14,9 @@ export default function Home() {
   if (user) console.log(user);
   return (
     <main>
-      <SinglePost />
+      {/* <SinglePost /> */}
       <Write />
-      <Posts />
+      {/* <Posts /> */}
       <HeroHighlight containerClassName="custom-container-class pt-32">
         <p className="mt-4 text-xl text-center">
           <Highlight className="text-indigo-500">Experience</Highlight> the best
