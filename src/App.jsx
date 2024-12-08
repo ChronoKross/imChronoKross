8ce40cb4aeeb8fc728a23d899490e47225da53c0
ChronoKross <nathanblainegwyn@gmail.com>
2024-12-08 06:09:00 -0500

    changed file casing
    freaks git out lol

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import { useContext } from "react";
 import RegisterForm from "./components/pages/register/RegisterForm";
-import Home from "./components/pages/Home/Home";
-import LoginForm from "./components/pages/Login/LoginForm";
+import Home from "./components/pages/home/Home";
+import LoginForm from "./components/pages/login/LoginForm";
 import Posts from "./components/features/blog/pages/Posts";
 import SinglePost from "./components/features/blog/pages/SinglePost";
 import PrivacyPolicy from "./components/legal/PrivacyPolicy";
+import GoogleOAuthRedirect from "./components/auth/GoogleOAuth/GoogleOAuthRedirect";
 
 function App() {
   const { user } = useContext(AuthContext);
@@ -44,7 +45,10 @@ function App() {
           <Route path="/login" element={<LoginForm />} />
           <Route path="/blog" element={<Posts />} />
           <Route path="/blog/:id" element={<SinglePost />} />
-          <Route path="/connect/${provider}/redirect" element={<Home />} />
+          <Route
+            path="/connect/:provider/redirect"
+            element={<GoogleOAuthRedirect />}
+          />
         </Routes>
       </div>
     </Router>
