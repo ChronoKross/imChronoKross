b74f3f3ced0cdf3793638d02c8a2c772b93bafb6
ChronoKross <nathanblainegwyn@gmail.com>
2024-12-08 22:49:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from "./context/AuthContext";
 import { useContext } from "react";
 import RegisterForm from "./components/Pages/Register/RegisterForm.jsx";
 import Home from "./components/Pages/home/Home.js";
-import LoginForm from "./components/Pages/login/LoginForm.js";
+import LoginForm from "./components/Pages/Login/LoginForm.js";
 import Posts from "./components/features/blog/pages/Posts";
 import SinglePost from "./components/features/blog/pages/SinglePost";
 import PrivacyPolicy from "./components/legal/PrivacyPolicy";
