a28ace824721ae0e7dd5e4dc1e82405b96b640f0
ChronoKross <nathanblainegwyn@gmail.com>
2024-12-13 05:00:02 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -62,12 +62,9 @@ function App() {
           <Route path="/login" element={<LoginForm />} />
           <Route path="/blog" element={<Posts />} />
           <Route path="/blog/:id" element={<SinglePost />} />
-          <Route
-            path="/connect/google/redirect"
-            element={<GoogleOAuthRedirect />}
-          />
-
           <Route path="*" element={<GoogleOAuthRedirect />} />
+
+          {/* <Route path="*" element={<GoogleOAuthRedirect />} /> */}
         </Routes>
 
         {/* SOME REASON THIS DOES NOT WORK {  <Route
